1a07cb43b576dcf6575392371aaa78bf7468df4a
John Anchit <doublealpha99@gmail.com>
2023-02-14 18:26:29 +0600

    Some string methods and if-else

--- DST01/DST01/Program.cs
+++ DST01/DST01/Program.cs
@@ -1,4 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-string name = "John Anchit";
+string name = "John  Anchit";
 Console.WriteLine("This is " + name);
+if(string.IsNullOrEmpty(name))
+{
+    Console.WriteLine("Empty");
+}
+else
+{
+    Console.WriteLine("Given string: " + name);
+}
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Empty");
+}
+else
+{
+    Console.WriteLine("Given string: " + name);
+}
+
