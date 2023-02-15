11cd11081485181dbc6e2d70631ec1ecb338e986
John Anchit <doublealpha99@gmail.com>
2023-02-15 20:30:19 +0600

    Simple getter setter practice

---
+++ Problem05/Problem05/Human.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem05
+{
+    internal class Human
+    {
+        private string _name;
+        public string Name{
+            get
+            {
+                return this._name.ToUpper();
+            }
+            set
+            {
+                this._name= value;
+            } 
+        }
+
+        public string Display()
+        {
+            return _name;
+        }
+    }
+}
---
+++ Problem05/Problem05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Problem05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h= new Human();
+            h.Name=Console.ReadLine();
+            Console.WriteLine("Name: "+h.Display());
+            Console.ReadLine();
+        }
+    }
+}
