35b17a7c525ffe291b2e6343c4ee26b8d491936c
John Anchit <doublealpha99@gmail.com>
2023-02-15 22:24:05 +0600

    Constructor and Destructor

--- DST06/DST06/Program.cs
+++ DST06/DST06/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace DST06
 {
@@ -10,6 +7,29 @@ namespace DST06
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student("John",23);
+            Student s2 = new Student();
+            Student s3 = new Student("Toto");
+
+            //s1.Name = "John";
+            s1.Display();
+            Console.WriteLine();
+            s2.Display();
+            Console.WriteLine();
+            s3.Display();
+            Console.WriteLine();
+
+            Close();
+            GC.Collect();       //GC used to call Destructor
+
+
+            Console.ReadLine();
+        }
+
+        static void Close()
+        {
+            Student S4 = new Student(20);
+            S4.Display();
         }
     }
 }
---
+++ DST06/DST06/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DST06
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+
+        public Student(int Age)
+        {
+            this.Age = Age;
+            Name = "John Doe";
+            //Console.WriteLine("CTOR is called.");
+        }
+        public Student(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        /* public Student()        //ctor + tab tab
+         {
+             Age = 18;
+             Name = "John Doe"; 
+         }*/
+        public Student() => (Age, Name) = (18, "John Doe");     //Same as the constructor above
+
+        public Student(string Name)
+        {
+            this.Name = Name;
+            this.Age = 18;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+        }
+        
+    }
+}
