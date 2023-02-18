4d2558210fc46f11093516b94e542c2593554862
John Anchit <doublealpha99@gmail.com>
2023-02-18 19:46:18 +0600

    Generic class practice

---
+++ DST11/DST11/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DST11
+{
+    interface ICalculator
+    {
+        void Display();
+    }
+    //internal class Calculator<T, U>    //Generic. any name, T mostly used
+
+    //{
+    /*public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    public double Sum(double a, double b)
+    {
+        return a + b;
+    }
+    public string Sum(string a, string b)
+    {
+        return a + b;
+    }*/
+
+    /* public T First { get; set; }
+     public U Second { get; set; }
+     //public dynamic First { get; set; }
+     //public dynamic Second { get; set; }
+
+     public Calculator(T first, U second)
+     {
+         this.First = first;
+         this.Second = second;
+         //this.First = 10;    //This cannot be done. Not able to forcefully convert to int and etc.
+
+     }
+
+     public U Sum()
+     {
+         dynamic d1 = First;     //any data type is adapted through dynamic
+         dynamic d2 = Second;
+         return d1 + d2;
+
+     }*/
+
+    /*public U Sum ()
+    {
+
+        return First + Second;
+    }*/
+    //}
+
+    internal class Calculator <T> : ICalculator where T : Human      //It means T must be Human
+    {
+        public Human human { get; set; }
+
+        public Calculator(T a)
+        {
+            human = a;  
+        }
+        public void Display()
+        {
+            Console.WriteLine("Age is: "+human.Age);
+        }
+    }
+}
---
+++ DST11/DST11/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DST11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Calculator<int, double> c1= new Calculator<int, double>(5,8.5);
+            var c2= new Calculator<double, double>(5.5, 9.8);       //works all the same
+            var c3= new Calculator<string, string>("John", "Vai");
+            Console.WriteLine("Sum = "+c1.Sum());
+            Console.WriteLine("Sum = "+c2.Sum());
+            Console.WriteLine("String adding = " + c3.Sum());*/
+
+            Human human= new Human();
+            human.Age= 18;
+            var c1 = new Calculator<Human>(human);
+
+
+            Console.ReadLine();
+        }
+    }
+}
