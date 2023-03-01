800b8417ed6c0b6cec149d0309908be3cfa5cac9
anchit15 <doublealpha99@gmail.com>
2023-03-01 14:46:12 +0600

    new setup commit

---
+++ DST15/DST15/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DST15
+{
+    internal class Student
+    {
+        public string StudentName { get; set; }
+        public float StudentCGPA { get; set; }
+    }
+}
